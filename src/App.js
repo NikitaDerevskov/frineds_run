866d938cdfb72951746602ade4d6a9c882c42612
hork <horkworse@gmail.com>
2020-06-20 02:49:45 +0300

    nothink fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { View, Panel, PanelHeader, Header, Group, Cell } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
-function App() {
-	return (<div>Hello Vanya</div>);
+function App () {
+  return (
+    <View activePanel="main">
+      <Panel id="main">
+        <PanelHeader>VKUI</PanelHeader>
+        <Group header={<Header mode="secondary">Items</Header>}>
+          <Cell>Hello</Cell>
+          <Cell>World</Cell>
+        </Group>
+      </Panel>
+    </View>
+  );
 }
 
 export default App;
