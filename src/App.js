3c92391ad9db9cd535a102fc3ebb26aaeb949f06
Nikita Derevskov <n.derevskov@examus.net>
2020-06-20 20:01:56 +0500

    from root to views and fixs

--- src/App.js
+++ src/App.js
@@ -10,37 +10,11 @@ import {
 import '@vkontakte/vkui/dist/vkui.css';
 import Bottompanel from "./bottompanel";
 import './styles/app.css'
-
+import StartPageView from './views/startPageView'
 
 
 function App () {
-    return (
-        <View activePanel="main">
-            <Panel id="main">
-                <PanelHeader>
-                    Статьи
-                </PanelHeader>
-                <CardGrid>
-                    <Card size="l">
-                        <div style={{ height: 200 }} />
-                    </Card>
-                    <Card size="l">
-                        <div style={{ height: 200 }} />
-                    </Card>
-                    <Card size="l">
-                        <div style={{ height: 200 }} />
-                    </Card>
-                    <Card size="l">
-                        <div style={{ height: 200 }} />
-                    </Card>
-                    <Card size="l">
-                        <div style={{ height: 200 }} />
-                    </Card>
-                </CardGrid>
-                <Bottompanel></Bottompanel>
-            </Panel>
-        </View>
-    );
+    return (<StartPageView />);
 }
 
 export default App;
--- src/views/articlesView.js
+++ src/views/articlesView.js
@@ -1,3 +1,50 @@
+// import React from 'react';
+// import ArticlesList from '../components/articlesList.js';
+// import ArticleItem from '../components/articleItem.js';
 import React from 'react';
-import ArticlesList from '../components/articlesList.js';
-import ArticleItem from '../components/articleItem.js';
+import {
+    View,
+    Panel,
+    PanelHeader,
+    Card,
+    CardGrid
+
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+import Bottompanel from "../bottompanel";
+import '../styles/app.css'
+
+
+
+function ArticleView () {
+    return (
+        <View activePanel="main">
+            <Panel id="main">
+                <PanelHeader>
+                    Статьи
+                </PanelHeader>
+                <CardGrid>
+                    <Card size="l">
+                        <div style={{ height: 200 }} />
+                    </Card>
+                    <Card size="l">
+                        <div style={{ height: 200 }} />
+                    </Card>
+                    <Card size="l">
+                        <div style={{ height: 200 }} />
+                    </Card>
+                    <Card size="l">
+                        <div style={{ height: 200 }} />
+                    </Card>
+                    <Card size="l">
+                        <div style={{ height: 200 }} />
+                    </Card>
+                </CardGrid>
+                <Bottompanel></Bottompanel>
+            </Panel>
+        </View>
+    );
+}
+
+export default ArticleView;
+
