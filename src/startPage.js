17070a426cb98877b80c68e3e7ffb5dcce85fab1
Nikita Derevskov <n.derevskov@examus.net>
2020-06-20 16:45:55 +0500

    Ready start screen

--- src/startPage.js
+++ src/startPage.js
@@ -15,6 +15,8 @@ import {
 
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
+import logo from '../src/start_image.png';
+import './styles/app.css'
 
 import Icon28NewsfeedOutline from '@vkontakte/icons/dist/28/newsfeed_outline';
 import Icon28Notifications from '@vkontakte/icons/dist/28/notifications';
@@ -30,7 +32,16 @@ function App () {
                 <PanelHeader left={<PanelHeaderBack onClick={() => console.log('hey')} />}>
                     FriendsRun
                 </PanelHeader>
-
+                <Div className = "image">
+                    <img src={logo} alt="logo" />
+                    <p>
+                        <strong >Привет дорогой друг!</strong>
+                    </p>
+                    <p className="startText">
+                        <br/>Это приложение поможет тебе держать себя в форме, а также находить новых друзей
+                    </p>
+                </Div>
+                <Button size="xl" className="startButton">Продолжить</Button>
                 <Tabbar>
                     <TabbarItem
                         onClick={() => console.log('hey')}
